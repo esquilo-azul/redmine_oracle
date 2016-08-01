6d27a8604e9df3284ef3c12c037a7cb27a7c285f
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2016-08-01 16:09:19 -0300

    Plugin "redmine_oracle". Refs #993.

---
+++ Gemfile
@@ -0,0 +1,2 @@
+gem 'activerecord-oracle_enhanced-adapter', '~> 1.6.0'
+gem 'ruby-oci8', '~> 2.1.0'
---
+++ init.rb
@@ -0,0 +1,12 @@
+# coding: utf-8
+
+require 'redmine'
+
+Redmine::Plugin.register :redmine_oracle do
+  name 'Redmine Oracle'
+  author 'TRF1 - SJAP - SEINF'
+  description 'Conector para o Oracle'
+  version '0.0.1'
+  url 'http://172.18.4.200/redmine/projects/redmine'
+  author_url 'http://172.18.4.200/redmine/projects/seinf-ap'
+end
