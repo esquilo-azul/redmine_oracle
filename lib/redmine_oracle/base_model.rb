b9722c591d838439dea51d7f5292f41b4f3525eb
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2016-09-01 18:32:41 -0300

    Usuário responsável por férias de funcionários Redmine. Refs #988.

--- lib/redmine_oracle/base_model.rb
+++ lib/redmine_oracle/base_model.rb
@@ -1,25 +1,9 @@
 module RedmineOracle
   class BaseModel < ActiveRecord::Base
     include ::RedmineOracle::EnhancedModel
+    include ::Eac::SimpleCache
     establish_connection :oracle
 
     self.abstract_class = true
-
-    def reloadable(key, &block)
-      key = key.to_s
-      return reloadable_data[key] if reloadable_data.key?(key)
-      reloadable_data[key] = block.call
-    end
-
-    private
-
-    def reloadable_data
-      @reloadable_data ||= {}
-    end
-
-    def reload
-      reloadable_data.clear
-      super
-    end
   end
 end
