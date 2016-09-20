ff2ba203b48117c4f74cb8326177c839372ac1da
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2016-09-20 15:32:15 -0300

    Método "data_import_key" para todos os models Oracle. Refs #1026.

--- lib/redmine_oracle/base_model.rb
+++ lib/redmine_oracle/base_model.rb
@@ -5,5 +5,13 @@ module RedmineOracle
     establish_connection :oracle
 
     self.abstract_class = true
+
+    def data_import_key
+      attrs = self.class.pk_constraint.columns_names.sort.map do |c|
+        attr = c.downcase.to_sym
+        [attr, self[attr]]
+      end
+      attrs.to_h.to_yaml
+    end
   end
 end
