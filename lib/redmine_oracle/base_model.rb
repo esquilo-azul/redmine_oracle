048cde1e7a4b28cfdf3658949689fae8064d33ce
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2016-12-13 11:58:35 -0300

    Renomeia método "data_import_key" para "id" em RedmineOracle::BaseModel. Refs #1041.

--- lib/redmine_oracle/base_model.rb
+++ lib/redmine_oracle/base_model.rb
@@ -6,7 +6,7 @@ module RedmineOracle
 
     self.abstract_class = true
 
-    def data_import_key
+    def id
       attrs = self.class.pk_constraint.columns_names.sort.map do |c|
         attr = c.downcase.to_sym
         [attr, self[attr]]
@@ -14,7 +14,7 @@ module RedmineOracle
       attrs.to_h.to_yaml
     end
 
-    def self.parse_data_import_key(key)
+    def self.parse_id(key)
       YAML.load(key)
     end
   end
