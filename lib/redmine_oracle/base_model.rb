10392b762fb55351231b982ac9f3cf00a25ef3f4
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2017-04-26 13:43:36 -0300

    ".find" para models Oracle com ID em formato Hash. Refs #1038.

--- lib/redmine_oracle/base_model.rb
+++ lib/redmine_oracle/base_model.rb
@@ -6,12 +6,34 @@ module RedmineOracle
 
     self.abstract_class = true
 
+    class << self
+      def find(id)
+        return super if primary_key.present?
+        h = YAML.load(id)
+        fail "ID \"#{h}\" is not a Hash" unless h.is_a?(Hash)
+        find_by_pk_columns(h) || fail("Not found with #{h}")
+      end
+
+      def pk_columns
+        pk_constraint.columns_names.sort.map { |c| c.downcase.to_sym }
+      end
+
+      private
+
+      def find_by_pk_columns(attrs)
+        attrs = attrs.with_indifferent_access
+        q = all
+        pk_columns.each do |c|
+          fail "#{c} is blank (#{attrs})" if attrs[c].blank?
+          q = q.where(c => attrs[c])
+        end
+        q.first
+      end
+    end
+
     # Models com chaves primárias de um único campo sobreescrevem este método.
     def id
-      attrs = self.class.pk_constraint.columns_names.sort.map do |c|
-        attr = c.downcase.to_sym
-        [attr, self[attr]]
-      end
+      attrs = self.class.pk_columns.map { |attr| [attr, self[attr]] }
       attrs.to_h.to_yaml
     end
 
